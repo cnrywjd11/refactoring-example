60c956e6a761ebbd81bdc32883aaf7fe6898d962
cnrywjd11 <cnrywjd11@gmail.com>
2023-04-05 18:10:13 +0900

    질의함수를 매개변수로 바꾸기 예제를 위한 실내온도 제어 시스템 예제 준비

---
+++ ch11/6.1.js
@@ -0,0 +1,13 @@
+class HeatingPlain {
+    get targetTemperature() {
+        if      (thermostat.selectedTemperature > this._max) return this._max;
+        else if (thermostat.selectedTemperature < this._min) return this._min;
+        else return thermostat.selectedTemperature;
+    }
+}
+
+let thePlan = new HeatingPlain();
+
+if      (thePlan.targetTemperature > thermostat.currentTemperature) setToHeat();
+else if (thePlan.targetTemperature < thermostat.currentTemperature) setToCool();
+else setOff();
