4bd020487dc96eec5395cff26cec3fc0ff4a3ea0
cnrywjd11 <cnrywjd11@gmail.com>
2023-04-05 15:43:51 +0900

    함수를 복제하고 질의 목적에 맞는 이름짓기

--- ch11/1.1.js
+++ ch11/1.1.js
@@ -13,3 +13,17 @@ function alertForMiscreants(people) {
     }
     return "";
 }
+
+function findMiscreant(people) {
+    for (const p of people) {
+        if (p === '조커') {
+            sendAlert();
+            return '조커';
+        }
+        if (p === '사루만') {
+            sendAlert();
+            return '사루만';
+        }
+    }
+    return "";
+}
