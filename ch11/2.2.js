ad1e2e6eae0c1ef91e760d2f02fa1a39f098d436
cnrywjd11 <cnrywjd11@gmail.com>
2023-04-05 16:14:32 +0900

    대역의 상한/하한을 호출하는 부분도 새로만든 함수로 대체

--- ch11/2.2.js
+++ ch11/2.2.js
@@ -4,9 +4,9 @@ function baseCharge(usage) {
     }
 
     const amount =
-        bottomBand(usage) * 0.03
-        + withinBand(usage) * 0.05
-        + topBand(usage) * 0.07;
+        withinBand(usage, 0, 100) * 0.03
+        + withinBand(usage, 100, 200) * 0.05
+        + withinBand(usage, 200, Infinity) * 0.07;
 
     return usd(amount);
 }
