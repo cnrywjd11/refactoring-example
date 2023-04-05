dcbbeafba9018c6c7e7c7598b807f3081b9bd1aa
cnrywjd11 <cnrywjd11@gmail.com>
2023-04-05 22:16:57 +0900

    클래스의 charge함수를 최상위 charge() 함수로 인라인하고, 클래스를 삭제한다

--- ch11/10.1.js
+++ ch11/10.1.js
@@ -1,13 +1,7 @@
-class ChargeCalculator {
-    charge(customer, usage, provider) {
-        const baseCharge = customer.baseRate * usage;
-        return baseCharge + provider.connectionCharge;
-    }
-}
-
 // 호출자
 monthCharge = charge(customer, usage, provider);
 
 function charge(customer, usage, provider) {
-    return new ChargeCalculator(customer, usage, provider).charge(customer, usage, provider);
+    const baseCharge = customer.baseRate * usage;
+    return baseCharge + provider.connectionCharge;
 }
