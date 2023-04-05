efca202100225664903d29860ac9c272b86dad6b
cnrywjd11 <cnrywjd11@gmail.com>
2023-04-05 15:51:57 +0900

    원래 변경함수에서 질의 관련 코드를 없앤다

--- ch11/1.1.js
+++ ch11/1.1.js
@@ -5,14 +5,14 @@ function alertForMiscreants(people) {
     for (const p of people) {
         if (p === '조커') {
             sendAlert();
-            return '조커';
+            return;
         }
         if (p === '사루만') {
             sendAlert();
-            return '사루만';
+            return;
         }
     }
-    return "";
+    return;
 }
 
 function findMiscreant(people) {
