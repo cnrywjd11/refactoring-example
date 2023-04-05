93887b2a0d53a9c1793b82c53f811db6164f2bfb
cnrywjd11 <cnrywjd11@gmail.com>
2023-04-05 17:12:04 +0900

    함수 옮기기(8.1절) 및 흉한 접두어 제거

--- ch11/4.2.js
+++ ch11/4.2.js
@@ -1,24 +1,18 @@
 class HeatingPlan {
     // ...
 
-    withinRange(bottom, top) {
-        return (
-            bottom >= this._temperatureRange.low && top >= this._temperatureRange.high
-        );
+    xxNewwithinRanage(tempRange) {
+        const low = tempRange.low;
+        const high = tempRange.high;
+        const isWithInRange = this.withinRanage(low, high);
+        return isWithInRange;
     }
 }
 
 let aPlan = new HeatingPlan();
 
 const tempRange = aRoom.daysTempRange;
-const isWithInRange = xxNewwithinRanage(aPlan, tempRange);
+const isWithInRange = aPlan.xxNewwithinRanage(aPlan, tempRange);
 if (!isWithInRange) {
     alerts.push('방 온도가 지정 범위를 벗어났습니다.');
-}
-
-function xxNewwithinRanage(aPlan, tempRange) {
-    const low = tempRange.low;
-    const high = tempRange.high;
-    const isWithInRange = aPlan.withinRanage(low, high);
-    return isWithInRange;
 }
