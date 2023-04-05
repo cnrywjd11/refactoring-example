f57d801813b3b00aa439f81e14242415978610a6
cnrywjd11 <cnrywjd11@gmail.com>
2023-04-05 15:44:21 +0900

    새 질의함수에서 부수효과 낳는 부분 제거하기

--- ch11/1.1.js
+++ ch11/1.1.js
@@ -17,11 +17,9 @@ function alertForMiscreants(people) {
 function findMiscreant(people) {
     for (const p of people) {
         if (p === '조커') {
-            sendAlert();
             return '조커';
         }
         if (p === '사루만') {
-            sendAlert();
             return '사루만';
         }
     }
