87f7f82b0dfc37c06e534ff56037e99523c3c72b
cnrywjd11 <cnrywjd11@gmail.com>
2023-04-05 16:52:10 +0900

    함수 추출하기

--- ch11/4.2.js
+++ ch11/4.2.js
@@ -11,9 +11,14 @@ class HeatingPlan {
 let aPlan = new HeatingPlan();
 
 const tempRange = aRoom.daysTempRange;
-const low = tempRange.low;
-const high = tempRange.high;
-const isWithInRange = aPlan.withinRanage(low, high);
+const isWithInRange = xxNewwithinRanage(aPlan, tempRange);
 if (!isWithInRange) {
     alerts.push('방 온도가 지정 범위를 벗어났습니다.');
+}
+
+function xxNewwithinRanage(aPlan, tempRange) {
+    const low = tempRange.low;
+    const high = tempRange.high;
+    const isWithInRange = aPlan.withinRanage(low, high);
+    return isWithInRange;
 }
