8e294b09c475aaf45b80834d3057481e369641c3
cnrywjd11 <cnrywjd11@gmail.com>
2023-04-05 16:43:29 +0900

    흉한 접두어 제거하고 호출자에도 반영한다. 죽은 코드를 제거한다.

--- ch11/4.1.js
+++ ch11/4.1.js
@@ -1,20 +1,14 @@
 class HeatingPlan {
     // ...
 
-    withinRange(bottom, top) {
-        return (
-            bottom >= this._temperatureRange.low && top >= this._temperatureRange.high
-        );
-    }
-
-    xxNEWwithinRange(aNumberRange) {
+    withinRange(aNumberRange) {
         return aNumberRange.low >= this._temperatureRange.low && aNumberRange.high >= this._temperatureRange.high
     }
 }
 
 let aPlan = new HeatingPlan();
 
-if (!aPlan.xxNEWwithinRange(aRoom.daysTempRange)) {
+if (!aPlan.withinRange(aRoom.daysTempRange)) {
     alerts.push('방 온도가 지정 범위를 벗어났습니다.');
 }
 
