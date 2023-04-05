5ecdc157ba068154bdc9be616a4cb05b79b3700c
cnrywjd11 <cnrywjd11@gmail.com>
2023-04-05 22:04:44 +0900

    더 가다듬기 : 복잡한 함수를 잘게 나누기. 모든 지역 변수를 필드로 바꾸고, scoreSmoking 함수를 추출한다

--- ch11/9.1.js
+++ ch11/9.1.js
@@ -10,26 +10,28 @@ class Scorer {
     }
 
     execute() {
-        let result = 0;
-        let healthLevel = 0;
-        let highMedicalRiskFlag = false;
+        this._result = 0;
+        this._healthLevel = 0;
+        this._highMedicalRiskFlag = false;
 
-        if (this._medicalExam.isSmoker) {
-            healthLevel += 10;
-            highMedicalRiskFlag = true;
-        }
+        this.scoreSmoking();
+        this._certificationGrade = 'regular';
 
-        let certificationGrade = 'regular';
-        if (
-            this._scoringGuide.stateWithLowCertification(this._candidate.originState)
-        ) {
-            certificationGrade = 'low';
-            result -= 5;
+        if (this._scoringGuide.stateWithLowCertification(this._candidate.originState)) {
+            this._certificationGrade = 'low';
+            this._result -= 5;
         }
 
         // 비슷한 코드가 한참 이어짐
-        result -= Math.max(healthLevel - 5, 0);
+        this._result -= Math.max(this._healthLevel - 5, 0);
 
-        return result;
+        return this._result;
+    }
+
+    scoreSmoking() {
+        if (this._medicalExam.isSmoker) {
+            this._healthLevel += 10;
+            this._highMedicalRiskFlag = true;
+        }
     }
 }
