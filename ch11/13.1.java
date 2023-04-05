9ff8c9bff9a8f36cd3648344e86a501a4b733d15
cnrywjd11 <cnrywjd11@gmail.com>
2023-04-06 03:47:26 +0900

    자원이 고갈되는 건 예상할 수 있으므로, 예외 처리로 대응하는 것은 옳지 않다. 고갈되었는지 검사하는 코드를 추가한다.

--- ch11/13.1.java
+++ ch11/13.1.java
@@ -1,16 +1,21 @@
 public ResourcePool get() {
     Resource result;
 
-    try {
-        result = avaliable.pop()
-        allocated.add(result);
-    } catch (NoSuchElementException e) {
+    if (available.isEmpty()) {
         result = Resource.create();
-        allocated.add(result)
+        allocated.add(result);
+    } else {
+        try {
+            result = avaliable.pop()
+            allocated.add(result);
+        } catch (NoSuchElementException e) {
+            result = Resource.create();
+            allocated.add(result)
+        }
     }
 
     return result;
 }
 
-private Deque<Resource> avaliable;
+private Deque<Resource> available;
 private List<Resource> allocated;
